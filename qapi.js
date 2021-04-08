7e52d3e2182bcd4ae69b16b66751ca9697af2708
Goalof <68434460+Goalof@users.noreply.github.com>
2021-04-09 05:58:55 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"606f95eab4bf81001fc1a88b",
+				"606f95eab4bf81001fc1a88d"
+			]
+		},
+		"606f95eab4bf81001fc1a88b": {
+			"id": "606f95eab4bf81001fc1a88b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"606f95eab4bf81001fc1a88d": {
+			"id": "606f95eab4bf81001fc1a88d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/components/EmbedHTML.js
@@ -0,0 +1,43 @@
+import React, { useRef, useLayoutEffect } from "react";
+import atomize from "@quarkly/atomize"; // Put your HTML here:
+
+const customHtml = `
+  <form name="contact" method="post">
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+        
+        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
+</form>
+        
+        `;
+
+const EmbedHTML = ({
+	children,
+	...props
+}) => {
+	const ref = useRef(null);
+	useLayoutEffect(() => {
+		ref.current.innerHTML = customHtml;
+	}, []);
+	return <div {...props} ref={ref} />;
+};
+
+export default atomize(EmbedHTML)({
+	name: "EmbedHTML",
+	normalize: true,
+	mixins: true
+});
